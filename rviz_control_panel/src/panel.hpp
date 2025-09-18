deca5f1c11971f6a11eb1f47097d192754a5bb4c
LazarevMike <mike.lazarev4321@gmail.com>
2025-09-18 20:34:08 +1000

    added move to UI in rviz

--- rviz_control_panel/src/panel.hpp
+++ rviz_control_panel/src/panel.hpp
@@ -4,62 +4,76 @@
 #include <QLabel>
 #include <QVBoxLayout>
 #include <QGridLayout>
-#include <QTextEdit>
-#include <QStyleFactory>
 #include <QCheckBox>
+#include <QLineEdit>
+#include <QDoubleValidator>
 
 #include <rviz_common/panel.hpp>
+#include <rviz_common/display_context.hpp>
+
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/bool.hpp>
 #include <nav2_msgs/action/navigate_to_pose.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
-#include <rviz_common/display_context.hpp>
+
+// (Keep the pluginlib include in the .cpp where you EXPORT_CLASS)
 #include <pluginlib/class_list_macros.hpp>
 
 namespace rviz_control_panel
 {
 
-    class ControlPanel : public rviz_common::Panel
-    {
-        Q_OBJECT
+class ControlPanel : public rviz_common::Panel
+{
+  Q_OBJECT
 
-    public:
-        ControlPanel(QWidget *parent = nullptr);
-        void onInitialize() override; // called when RViz has a context/ROS node
-        void save(rviz_common::Config config) const override;
-        void load(const rviz_common::Config &config) override;
+public:
+  explicit ControlPanel(QWidget *parent = nullptr);
+  void onInitialize() override;                 // called when RViz has a context/ROS node
+  void save(rviz_common::Config config) const override;
+  void load(const rviz_common::Config &config) override;
 
-    private Q_SLOTS:
-        void onEstopClicked();
-        void onReturnBaseClicked();
-        void onManualControlToggled(bool enabled);
+private Q_SLOTS:
+  void onEstopClicked();
+  void onReturnBaseClicked();
+  void onManualControlToggled(bool enabled);
 
-    private:
-        // UI
-        QPushButton *btn_estop_{nullptr};
-        QPushButton *btn_rtb_{nullptr};
-        QLabel *status_{nullptr};
+  // New: execute "Inspect location" (send Nav2 goal to typed X/Y)
+  void onInspectExecute();
 
-        QPushButton *btn_forward_{nullptr};
-        QPushButton *btn_backward_{nullptr};
-        QPushButton *btn_left_{nullptr};
-        QPushButton *btn_right_{nullptr};
-        QPushButton *btn_stop_{nullptr};
-        QPushButton *btn_f_left_{nullptr};
-        QPushButton *btn_f_right_{nullptr};
-        QPushButton *btn_b_left_{nullptr};
-        QPushButton *btn_b_right_{nullptr};
+private:
+  // -------- UI: status + main controls --------
+  QLabel *status_{nullptr};
+  QPushButton *btn_estop_{nullptr};
+  QPushButton *btn_rtb_{nullptr};
 
-        QCheckBox *cb_manual_control_{nullptr};
-        QWidget *manual_control_group_{nullptr};
+  // -------- UI: Inspect location (X/Y + Execute) --------
+  QLineEdit *edit_x_{nullptr};
+  QLineEdit *edit_y_{nullptr};
+  QPushButton *btn_inspect_exec_{nullptr};
+  QDoubleValidator *xy_validator_{nullptr};
 
-        // ROS
-        rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr estop_pub_;
-        using NavigateToPose = nav2_msgs::action::NavigateToPose;
-        rclcpp_action::Client<NavigateToPose>::SharedPtr nav_client_;
+  // -------- UI: Manual control grid --------
+  QCheckBox *cb_manual_control_{nullptr};
+  QWidget *manual_control_group_{nullptr};
+  QPushButton *btn_forward_{nullptr};
+  QPushButton *btn_backward_{nullptr};
+  QPushButton *btn_left_{nullptr};
+  QPushButton *btn_right_{nullptr};
+  QPushButton *btn_stop_{nullptr};
+  QPushButton *btn_f_left_{nullptr};
+  QPushButton *btn_f_right_{nullptr};
+  QPushButton *btn_b_left_{nullptr};
+  QPushButton *btn_b_right_{nullptr};
 
-        // Settings (persisted to .rviz)
-        double home_x_{0.0}, home_y_{0.0}, home_yaw_deg_{0.0};
-    };
+  // -------- ROS interfaces --------
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr estop_pub_;
+  using NavigateToPose = nav2_msgs::action::NavigateToPose;
+  rclcpp_action::Client<NavigateToPose>::SharedPtr nav_client_;
+
+  // -------- Settings (persisted to RViz config) --------
+  double home_x_{0.0};
+  double home_y_{0.0};
+  double home_yaw_deg_{0.0};
+};
 
 } // namespace rviz_control_panel
