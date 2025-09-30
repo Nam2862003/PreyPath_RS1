aecd35208e751e83069a7ca36971e87b08773f9e
Nam2862003 <hoangnam2862003@gmail.com>
2025-09-30 09:34:02 +0000

    Cleanup Navigation branch

--- go2_description/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/git/PreyPath_RS1/go2_description
-# Build directory: /home/student/git/PreyPath_RS1/go2_description/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
